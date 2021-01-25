12294a1987e7f5228e2f1a2fd749cdebd18b8ac3
Tidarak <s.tidarak@gmail.com>
2021-01-26 06:46:37 +0700

    [*] update register seller

--- Seller/src/main/java/com/example/Seller/Controller/RegisterController.java
+++ Seller/src/main/java/com/example/Seller/Controller/RegisterController.java
@@ -1,4 +1,25 @@
 package com.example.Seller.Controller;
 
+import com.example.Seller.Entity.RegisterEntity;
+import com.example.Seller.Service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/seller")
 public class RegisterController {
+    @Autowired
+    private RegisterService registerService;
+
+    //@PostMapping(value = "register", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "register", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RegisterEntity> registerSeller(@RequestBody RegisterEntity request){
+        //registerService.saveMerchant(request);
+        return registerService.saveMerchant(request);
+    }
 }
--- Seller/src/main/java/com/example/Seller/Entity/RegisterEntity.java
+++ Seller/src/main/java/com/example/Seller/Entity/RegisterEntity.java
@@ -1,4 +1,38 @@
 package com.example.Seller.Entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
 public class RegisterEntity {
+    @Id
+    private int merchant_id;
+
+    @JsonProperty("merchant_name")
+    private String merchant_name;
+
+    @JsonProperty("merchant_email")
+    private String merchant_email;
+
+    @JsonProperty("merchant_password")
+    private String merchant_password;
+
+    @JsonProperty("merchant_tel")
+    private String merchant_tel;
+
+    @JsonProperty("merchant_account_no")
+    private String merchant_account_no;
+
+    @JsonProperty("merchant_account_name")
+    private String merchant_account_name;
+
+    @JsonProperty("merchant_bank")
+    private String merchant_bank;
+
+    @JsonProperty("merchant_prompt_pay")
+    private String merchant_prompt_pay;
+
+    @JsonProperty("merchant_status")
+    private String merchant_status;
 }
